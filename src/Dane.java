e973f65fe61a377969cb9b76bd4a5eb624275446
user <aa@aa.pl>
2025-05-30 14:06:05 +0200

    gotowe

---
+++ src/Dane.java
@@ -0,0 +1,8 @@
+public class Dane extends Exception{
+    public Dane() {
+    }
+
+    public Dane(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,65 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        int typ, bok;
-        double wysokosc;
-        try
-        {
-            //File plik = new File("dane.txt");
-            Scanner dane = new Scanner(new File("C:\\Users\\user\\Documents\\zadanie 2\\src\\dane.txt"));
-            while(dane.hasNextLine())
-            {
-                typ = Integer.parseInt(dane.next());
-                if(typ==1)
-                {
-                    bok = Integer.parseInt(dane.next());
-                    Kwadrat kw = new Kwadrat(bok);
-                    System.out.println(kw.pole());
-                } else if (typ==2) {
-                    bok = Integer.parseInt(dane.next());
-                    wysokosc = Integer.parseInt(dane.next());
-                    Prostopadloscian p = new Prostopadloscian(bok, wysokosc);
+        String linia;
+        String[] wartosci;
+        try {
+            Scanner dane = new Scanner(new File("C:\\Users\\user\\Documents\\zadanie-2-na-6\\src\\dane.txt"));
+            while (dane.hasNextLine()) {
+                try {
+                    linia = dane.nextLine();
+                    wartosci = linia.split(" ");
+                    if (Integer.parseInt(wartosci[0]) == 1) {
+                        if(wartosci.length>2  || wartosci.length<2)
+                        {
+
+                            System.err.println(new Dane("Za dużo lub za mało wartości dla pobranego typu figury!"));
+                        }
+                        else
+                        {
+                            if(Integer.parseInt(wartosci[1])>0)
+                            {
+                                Kwadrat kw = new Kwadrat(Integer.parseInt(wartosci[1]));
+                                System.out.println("Kwadrat: "+"bok="+wartosci[1]+", pole: "+kw.pole());
+                            }
+                            else
+                            {
+                                System.err.println(new Dane("Długość boku w kwadracie ujemna lub zerowa!"));
+                            }
+
+                        }
+                    } else if (Integer.parseInt(wartosci[0]) == 2) {
+
+                        if(wartosci.length>3  || wartosci.length<3)
+                        {
+                            System.err.println(new Dane("Za dużo lub za mało wartości dla pobranego typu figury!"));
+                        }
+                        else
+                        {
+                            if(Integer.parseInt(wartosci[1])>0 && Integer.parseInt(wartosci[2])>0)
+                            {
+                                Prostopadloscian p = new Prostopadloscian(Integer.parseInt(wartosci[1]), Integer.parseInt(wartosci[2]));
+                                System.out.println("Prostopadłościan: "+"bok="+wartosci[1]+", wysokość="+wartosci[2]+", pole: "+p.pole());
+                            }
+                            else
+                            {
+                                System.err.println(new Dane("Długość boku lub wysokości w prostopadłościanie ujemna lub zerowa!"));
+                            }
+
+                        }
+                    } else {
+                        System.err.println(new Dane("Błędny typ"));
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println(new Dane("Oczekiwano liczby"));
                 }
-                else
-                {
-                    System.out.println("Błąd");
-                }
+                //System.out.println(typ);
+
             }
         }
         catch (FileNotFoundException e)
         {
-            System.err.println("Plik nie został znaleziony: " + e.getMessage());
+            System.err.println(new Dane("Plik nie został znaleziony"));
         }
-
-
-
-        Prostopadloscian a = new Prostopadloscian();
-        System.out.println(a.pole());
     }
 }
